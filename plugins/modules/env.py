21065a6ad6d8e11471303046327e43738f76b50d
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2023-05-02 07:09:15 +0100

    env: add config module argument
    Signed-off-by: Pedro Tôrres <t0rr3sp3dr0@gmail.com>

--- plugins/modules/env.py
+++ plugins/modules/env.py
@@ -15,15 +15,19 @@ description: >
   The install operation creates a new user environment, based on the current
   generation of the active profile, to which a set of store paths described by
   args is added.
-short_description: nix-env -f ./defexpr.nix -ir
-version_added: 0.0.1
+short_description: manipulate or query Nix user environments
+version_added: 0.0.2
 author:
   - Pedro Tôrres (@t0rr3sp3dr0)
 extends_documentation_fragment:
   - community.general.attributes
 options:
-  derivations:
-    description: List of packages by channel.
+  config:
+    description: Channels config set.
+    default: '{}'
+    type: str
+  packages:
+    description: Package names by channel.
     required: true
     type: dict[str,list[str]]
 '''
@@ -32,12 +36,16 @@ options:
 EXAMPLES = r'''
 - name: nix_env
   t0rr3sp3dr0.nix.env:
-    derivations:
+    config: |
+      {
+        allowUnfree = true;
+      }
+    packages:
       nixpkgs:
-        - bash
-        - git
-      nixpkgs-unstable:
-        - python
+        - hello
+      t0rr3sp3dr0:
+        - intellij-idea-community-edition
+        - visual-studio-code
 '''
 
 
@@ -53,13 +61,39 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.common.validation import (check_type_dict, check_type_list, check_type_str)
 
 
+class CheckTypeConfig:
+    def __init__(self):
+        self.__name__ = 'str'
+
+    def __call__(self, config):
+        return check_type_str(config)
+
+
+class CheckTypePackages:
+    def __init__(self):
+        self.__name__ = 'dict[str,list[str]]'
+
+    def __call__(self, packages):
+        packages = check_type_dict(packages)
+        for channel, names in packages.items():
+            channel = check_type_str(channel)
+            names = check_type_list(names)
+            for name in names:
+                name = check_type_str(name)
+        return packages
+
+
 class NixEnv:
     def __init__(self):
         self.module = AnsibleModule(
             argument_spec={
-                'derivations': {
-                    'type': NixEnv.__check_type_derivations,
+                'config': {
+                    'default': '{}',
+                    'type': CheckTypeConfig(),
+                },
+                'packages': {
                     'required': True,
+                    'type': CheckTypePackages(),
                 },
             },
             supports_check_mode=True,
@@ -69,36 +103,28 @@ class NixEnv:
             'changed': False
         }
 
-    @staticmethod
-    def __check_type_derivations(derivations):
-        derivations = check_type_dict(derivations)
-        for channel, names in derivations.items():
-            channel = check_type_str(channel)
-            names = check_type_list(names)
-            for name in names:
-                name = check_type_str(name)
-        return derivations
+    def make_defexpr(self, config=None, packages=None):
+        if config is None:
+            config = self.module.params['config']
+        if packages is None:
+            packages = self.module.params['packages']
 
-    def make_defexpr(self):
-        DEFEXPR_NIX = textwrap.dedent('''\
+        lblock = []
+        iblock = []
+        for channel, names in packages.items():
+            lexpr = '{channel} = import <{channel}> {config};'.format(channel=channel, config=config)
+            lblock.append(lexpr)
+            iexpr = 'inherit ({channel}) {names};'.format(channel=channel, names=' '.join(names))
+            iblock.append(iexpr)
+
+        content = textwrap.dedent('''\
             let
               {lblock}
             in {{
               {iblock}
             }}
         ''')
-
-        lblock = []
-        iblock = []
-
-        derivations = self.module.params['derivations']
-        for channel, packages in derivations.items():
-            lexpr = '{channel} = import <{channel}> {{}};'.format(channel=channel)
-            lblock.append(lexpr)
-            iexpr = 'inherit ({channel}) {packages};'.format(channel=channel, packages=' '.join(packages))
-            iblock.append(iexpr)
-
-        content = DEFEXPR_NIX.format(lblock='\n  '.join(lblock), iblock='\n  '.join(iblock))
+        content = content.format(lblock='\n  '.join(lblock), iblock='\n  '.join(iblock))
         content = content.encode('utf-8')
 
         try:
@@ -109,12 +135,16 @@ class NixEnv:
             msg = str(e)
             self.module.fail_json(msg)
 
-    def run_install(self, defexpr_path):
-        bin_path = self.module.get_bin_path('nix-env', required=True, opt_dirs=['/nix/var/nix/profiles/default/bin'])
+    def run_install(self, defexpr_path=None):
+        if defexpr_path is None:
+            defexpr_path = self.make_defexpr()
+
         dry_run = ('--dry-run') if self.module.check_mode else ()
 
-        argv = [bin_path, *dry_run, '-f', defexpr_path, '-ir']
-        _, stdout, stderr = self.module.run_command(argv, check_rc=True)
+        args = [self.bin_path, *dry_run, '-f', defexpr_path, '-ir']
+        self.module.log("'{}'".format("' '".join(args)))
+
+        _, stdout, stderr = self.module.run_command(args, check_rc=True)
         self.module.debug(stdout)
         self.module.debug(stderr)
 
@@ -122,8 +152,8 @@ class NixEnv:
             self.result['changed'] = True
 
     def run(self):
-        defexpr_path = self.make_defexpr()
-        self.run_install(defexpr_path)
+        self.bin_path = self.module.get_bin_path('nix-env', required=True, opt_dirs=['/nix/var/nix/profiles/default/bin'])
+        self.run_install()
         self.module.exit_json(**self.result)
 
 
